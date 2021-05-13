772c9d1bc1ba251dc68d40aa9cdfd3cc36ef7ac5
Marko Plavljanin <mplavljanin@hotmail.com>
2021-05-13 19:51:48 +0200

    added some new functionalities to my app

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE.URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now())
+
+    def repr(self):
+        return f'<Task {self.id}>'
 
 
 @app.route('/')
