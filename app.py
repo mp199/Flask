54a1ae9241541e6b938a6f94233a80549bc884c2
Marko Plavljanin <mplavljanin@hotmail.com>
2021-05-20 19:35:30 +0200

    Added some new changes on my app

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE.URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
@@ -11,14 +11,54 @@ class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.now())
-
-    def repr(self):
-        return f'<Task {self.id}>'
+    date_modified = db.Column(db.DateTime, default=datetime.now())
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']    # uzimamo sadržaj iz input elementa. U index.html text-input
+        new_task = ToDo(content=task_content)     # element ima naziv content
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue with adding your record to database. Please try again later...'
+
+    else:
+        tasks = ToDo.query.order_by(ToDo.date_created).all()
+    return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = ToDo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem with deleting your record, please try again...'
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = ToDo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+        task.date_modified = datetime.now()
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was a problem with updating your task, please try again...'
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == '__main__':
